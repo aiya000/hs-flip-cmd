4191c534401767dd8d202e25aa71c50065a85698
aiya000 <aiya000.develop@gmail.com>
2018-02-25 21:47:42 +0900

    :tada: できた

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,23 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Control.Exception.Safe (MonadCatch, SomeException, catch)
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.Process (callCommand)
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = suppressErrMsg body
+  where
+    catch' :: MonadCatch m => m a -> (SomeException -> m a) -> m a
+    catch' = catch
+
+    suppressErrMsg :: IO () -> IO ()
+    suppressErrMsg m = m `catch'` const exitFailure
+
+    body :: IO ()
+    body = getArgs >>= \case
+      (cmd:x1:x2:xs) -> callCommand $ unwords (cmd:x2:x1:xs)
+      xs             -> callCommand $ unwords xs
